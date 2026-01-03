7b8a32b1c60f4776d22b1adde38576655070a49e
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-03 22:30:08 +0600

    Updated

--- src/script.ts
+++ src/script.ts
@@ -7,8 +7,6 @@ console.log(playName);
 console.log(age);
 
 // array
-
-
 // let fruits = ['apple', 34, true]; here we can push mixed values
 let fruits = ['apple', 'banana', 'orange'];
 // fruits.push(40); we cant push number to our string array
